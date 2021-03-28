9d0e292b09d77c6692b60e0bb9b25290310e552f
ya83 <admin@yurialexeev.com>
2021-03-29 01:56:35 +0300

    Added correctly animation to categories, modify js and scss files

--- js/filter.dev.js
+++ js/filter.dev.js
@@ -1,9 +1,12 @@
+"use strict";
+
 filterSelection("all");
 
 function filterSelection(c) {
-  let x, i;
+  var x, i;
   x = document.getElementsByClassName("filterDiv");
   if (c == "all") c = "";
+
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
@@ -11,9 +14,10 @@ function filterSelection(c) {
 }
 
 function w3AddClass(element, name) {
-  let i, arr1, arr2;
+  var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
+
   for (i = 0; i < arr2.length; i++) {
     if (arr1.indexOf(arr2[i]) == -1) {
       element.className += " " + arr2[i];
@@ -22,36 +26,39 @@ function w3AddClass(element, name) {
 }
 
 function w3RemoveClass(element, name) {
-  let i, arr1, arr2;
+  var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
+
   for (i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
       arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
   }
+
   element.className = arr1.join(" ");
 }
 
-let btnContainer = document.getElementById("btnsWrapper");
-let btns = btnContainer.getElementsByClassName("btns");
+var btnContainer = document.getElementById("btnsWrapper");
+var btns = btnContainer.getElementsByClassName("btns");
+
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
-    let current = document.getElementsByClassName("active");
+    var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
 }
 
-let buttons = document.querySelectorAll('.btns');
-let fDiv= document.querySelectorAll('.filterDiv');
+var buttons = document.querySelectorAll('.btns');
+var fDiv = document.querySelectorAll('.filterDiv');
 buttons.forEach(function (btn) {
   btn.addEventListener('click', function () {
-    fDiv.forEach((show) => {
+    fDiv.forEach(function (show) {
       show.classList.remove('fade-in');
-      setTimeout(() => { 
+      setTimeout(function () {
         show.classList.add('fade-in');
-      },100);
+      }, 100);
     });
   });
 });
